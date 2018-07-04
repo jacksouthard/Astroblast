da83914e10418ecf765ccba0cb668ad0b1626bb6
Jack Southard <jacksouthard265@gmail.com>
2018-07-04 10:08:26 -0700

    Surface graphical updates and ship hover animation

---
+++ Assets/Starfield.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Starfield : MonoBehaviour {
+	public GameObject starPrefab;
+	public int starCount;
+	public float width;
+	public float height;
+
+	void Start () {
+		for (int i = 0; i < starCount; i++) {
+			Vector3 localPos = new Vector3 (Random.Range (-width, width), Random.Range (-height, height), 10f);
+			Instantiate (starPrefab, transform.TransformPoint (localPos), Quaternion.identity, transform);
+		}
+	}
+}
